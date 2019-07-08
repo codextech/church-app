51e47749f62cb0753fb973917a1ea6406fb27aec
tanzeel850 <tanzeelsaleem10@gmail.com>
2019-07-09 00:49:56 +0500

    initial angular project

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,8 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingLayoutModule } from './layout/landing-layout/landing-layout.module';
+import { AdminLayoutModule } from './layout/admin-layout/admin-layout.module';
+import { MainModule } 
